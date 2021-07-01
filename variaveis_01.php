5074f7fe2226a103bf8c39fcb79415222ef125db
Carlos <Carlos.admcode@gmail.com>
2021-07-01 14:43:53 -0300

    Cria novo arquivo para conseitos de variaveis

---
+++ variaveis_01.php
@@ -0,0 +1,15 @@
+<?php
+	$nome = "Glaucio";
+	//
+	function teste_00()// Função em php
+	{
+		echo $nome;
+	}
+	//
+	function teste_01()
+	{
+
+	}
+	//
+	teste_00();// Chamada de execução da função 
+?>
