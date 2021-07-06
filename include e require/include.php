2fd00ba3433a83feebfebb0416ea47df5b232380
Carlos <Carlos.admcode@gmail.com>
2021-07-06 14:39:59 -0300

    Codigos de include e require, prepara aquivos para aula de estrutura de controle

---
+++ include e require/include.php
@@ -0,0 +1,9 @@
+<?php
+	// iclude busca o arquivo na pasta caso não exista ele busca em um diretorio padrão pre configurado 
+	// util para programas intranet
+	include "funcao_util.php";
+	//
+	$resultado = somar(10, 20);
+	echo "$resultado";
+	//
+?>
---
+++ include e require/require.php
@@ -0,0 +1,9 @@
+<?php
+	// require so funciona se existir o arquivo na pasta
+	require_once "funcao_util.php"; // evita erro caso o arq seja chamado duas vezes
+	require "funcao_util.php";
+	//
+	$resultado = somar(10, 20);
+	echo "$resultado";
+	//
+?>
