fb604d67f87c12508b00356db8d26d3cd8bd22a0
Carlos <Carlos.admcode@gmail.com>
2021-07-08 14:17:18 -0300

    Introdução ao Json
    Conceitos iniciais do uso do Json vinculado ao uso de array

---
+++ Json/json.php
@@ -0,0 +1,8 @@
+<?php 
+	$pessoas = array();
+	//
+	array_push($pessoas, array('nome' => 'João', 'idade' => 20));
+	array_push($pessoas, array('nome' => 'Glacio', 'idade' => 25));
+	
+	echo json_encode($pessoas);
+?>
